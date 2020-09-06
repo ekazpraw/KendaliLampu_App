7217038f8ef0c3e296098310e7716ae4bb80d185
Eka Prawira, S.Kom <ekazprawira@gmail.com>
2020-09-07 05:28:24 +0700

    Initial commit

---
+++ app/src/main/java/com/on29studio/ekazpraw/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.on29studio.ekazpraw;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutActivity extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
+    }
+}
